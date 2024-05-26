9d322d3c63ee934a508cf938c7fd2b1169dfc81c
NeuroWhAI <tlsehdgus0212@gmail.com>
2024-05-26 12:24:28 +0900

    🚸 Reduce trigger time

--- src/main.ts
+++ src/main.ts
@@ -139,7 +139,7 @@ client.on(Events.MessageCreate, async (msg) => {
     const agentRunning = agentManager.checkRunning(channelId);
     const triggerTime = agentRunning
       ? 8 * 1000 + Math.floor(4 * 1000 * Math.random())
-      : 60 * 1000 + Math.floor(3600 * 1000 * Math.random());
+      : 60 * 1000 + Math.floor(30 * 60 * 1000 * Math.random());
 
     triggerId = setTimeout(async () => {
       chatTriggers.delete(channelId);
